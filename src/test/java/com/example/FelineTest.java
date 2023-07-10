9d283bfde018b8c2fa90d4284adc3de363c62dc2
Ella Yurochko <bcerga.ycnex@yandex.com>
2023-07-10 12:23:02 +0300

    fixed FelineTest

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -2,10 +2,6 @@ package com.example;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
